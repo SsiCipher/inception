5f6eb41e7dc30db001f2d09c9124cbc9ccb7a64c
ssicipher <cipher@mail.com>
2023-01-27 08:21:21 -0500

    FINISHED: FTP Server

--- src/requirements/bonus/ftp/tools/setup_ftp.sh
+++ src/requirements/bonus/ftp/tools/setup_ftp.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+
+if [ ! -f /var/log/vsftpd/vsftpd.log ]; then
+	mkdir -p /var/log/vsftpd/ && touch /var/log/vsftpd/vsftpd.log
+fi
+
+adduser -D $FTP_USER
+echo "$FTP_USER:$FTP_PASS" | chpasswd &>/dev/null
+chown -R $FTP_USER:$FTP_USER /var/www
+echo $FTP_USER | tee -a /etc/vsftpd.userlist &>/dev/null
+
+echo "FTP server has started successfully"
+
+vsftpd /etc/vsftpd/vsftpd.conf
