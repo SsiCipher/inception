404451e673b7d7a838557e1677cf235d067ce331
wer <wer>
2023-01-31 09:08:40 -0500

    UPDATE

--- src/requirements/bonus/redis/tools/setup_redis.sh
+++ src/requirements/bonus/redis/tools/setup_redis.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# if [ ! -f /etc/redis.conf.old ]; then
-# 	cp /etc/redis.conf redis.conf.old
-# 	sed 's|maxmemory 2mb' /etc/redis.conf
-# 	sed 's|maxmemory-policy allkeys-lru' /etc/redis.conf
-# fi
-
-echo "Redis startes successfully"
+echo "Redis started successfully"
 
 redis-server --protected-mode no
